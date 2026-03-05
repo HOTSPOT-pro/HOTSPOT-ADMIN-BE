36e78b1491ac7d6dce5dccd71b8bea4375d06f8d
andrew <leegd120@gmail.com>
2026-03-05 16:21:17 +0900

    feat : 개인 데이터 및 선물 데이터 조회에 필요한 util 클래스 구현

---
+++ src/main/java/hotspot/admin/usage/subscriptionUsage/infrastructure/keybuilder/SubscriptionUsageRedisKeyBuilder.java
@@ -0,0 +1,47 @@
+package hotspot.admin.usage.subscriptionUsage.infrastructure.keybuilder;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public final class SubscriptionUsageRedisKeyBuilder {
+
+    private static final DateTimeFormatter yyyyMM =
+            DateTimeFormatter.ofPattern("yyyyMM");
+
+    private static final DateTimeFormatter yyyyMMdd =
+            DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    private SubscriptionUsageRedisKeyBuilder() {}
+
+    private static String formatYYYYMM(LocalDate date) {
+        return date.format(yyyyMM);
+    }
+
+    private static String formatYYYYMMDD(LocalDate date) {
+        return date.format(yyyyMMdd);
+    }
+
+    public static String planLimit(Long subId) {
+        return "limit:sub:" + subId;
+    }
+
+    public static String planUsageMonth(Long subId, LocalDate date) {
+        return "usage:sub:" + subId + ":" + formatYYYYMM(date);
+    }
+
+    public static String planUsageDay(Long subId, LocalDate date) {
+        return "usage:sub:" + subId + ":" + formatYYYYMMDD(date);
+    }
+
+    public static String giftIndex(Long subId, LocalDate date) {
+        return "idx:gift:" + subId + ":" + formatYYYYMM(date);
+    }
+
+    public static String giftLimit(Long subId, Long giftId, LocalDate date) {
+        return "limit:gift:" + subId + ":" + giftId + ":" + formatYYYYMM(date);
+    }
+
+    public static String giftUsage(Long subId, Long giftId, LocalDate date) {
+        return "usage:gift:" + subId + ":" + giftId + ":" + formatYYYYMM(date);
+    }
+}
