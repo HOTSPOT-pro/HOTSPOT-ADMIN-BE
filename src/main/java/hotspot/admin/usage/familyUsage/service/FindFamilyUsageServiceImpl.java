cd53397ed2484f06a217515bd39176b1bf7751b0
andrew <leegd120@gmail.com>
2026-03-05 16:15:10 +0900

    feat : 가족데이터 사용량 service 계층 구현

---
+++ src/main/java/hotspot/admin/usage/familyUsage/service/FindFamilyUsageServiceImpl.java
@@ -0,0 +1,39 @@
+package hotspot.admin.usage.familyUsage.service;
+
+import hotspot.admin.usage.familyUsage.controller.port.FindFamilyUsageService;
+import hotspot.admin.usage.familyUsage.controller.response.FamilyUsageResponse;
+import hotspot.admin.usage.familyUsage.domain.FamilyUsage;
+import hotspot.admin.usage.familyUsage.domain.mapper.FamilyUsageMapper;
+import hotspot.admin.usage.familyUsage.infrastructure.respository.FamilySubscriptionJdbcRepository;
+import hotspot.admin.usage.familyUsage.service.port.FamilyUsageRepository;
+import hotspot.admin.usage.familyUsage.service.schema.FamilySubList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FindFamilyUsageServiceImpl implements FindFamilyUsageService {
+
+    private final FamilyUsageRepository findFamilyUsageRepository;
+    private final FamilySubscriptionJdbcRepository familySubscriptionJdbcRepository;
+    private final Clock clock;
+
+    @Transactional(readOnly = true)
+    @Override
+    public FamilyUsageResponse findFamilyUsage(Long familyId) {
+
+        List<FamilySubList> familySubList =
+                familySubscriptionJdbcRepository.findByFamilyId(familyId);
+
+        FamilyUsage familyUsage = findFamilyUsageRepository.findFamilyUsage(familyId, familySubList);
+
+        LocalDateTime now = LocalDateTime.now(clock);
+
+        return FamilyUsageMapper.toFamilyUsageResponse(familyUsage, familySubList, now);
+    }
+}
