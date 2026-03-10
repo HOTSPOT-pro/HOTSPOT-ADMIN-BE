659bffb39b9cf59a088e4f86bf71a4adbd05f746
Andrew <98813748+starboxxxx@users.noreply.github.com>
2026-03-10 10:56:49 +0900

    Update SecurityConfig.java

--- src/main/java/hotspot/admin/common/config/SecurityConfig.java
+++ src/main/java/hotspot/admin/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package hotspot.admin.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,6 +32,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/health").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
